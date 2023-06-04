5c593afae31c075e907c68dbd9d123470047df28
Gray <grayzprogrammer@gmail.com>
2023-06-03 19:05:00 -0500

    create an iOS application

---
+++ BrainMath/BrainMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainMathApp.swift
+//  BrainMath
+//
+//  Created by G Zhen on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BrainMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
