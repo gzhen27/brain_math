6edb0fcc02932132027f07066a12ec19fe3d14c7
Gray Zhen <grayzprogrammer@gmail.com>
2023-06-03 20:13:37 -0500

    BM-0002 | use TabView and sets up pages (#2)

---
+++ BrainMath/Views/SettingView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingView.swift
+//  BrainMath
+//
+//  Created by G Zhen on 6/3/23.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Setting View")
+            Spacer()
+            Divider()
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
