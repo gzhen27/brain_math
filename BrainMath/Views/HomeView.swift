af2d32297f7b150e4e82c3ae0264ca4aa1d893c4
Gray <grayzprogrammer@gmail.com>
2023-06-10 18:51:47 -0500

    BM-0004 | use @appStorage to store user settings

--- BrainMath/Views/HomeView.swift
+++ BrainMath/Views/HomeView.swift
@@ -29,7 +29,6 @@ struct HomeView: View {
                     .environment(\.symbolVariants, .none)
             }
         }
-        .foregroundColor(CustomColor.tintColor)
         .onAppear {
             customizeTabViewStyle()
         }
--- BrainMath/Views/SettingView.swift
+++ BrainMath/Views/SettingView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct SettingView: View {
+    @AppStorage("trainModeSetting") var trainModeData = TrainModelSetting().encode()!
+    @State private var trainModeSetting = TrainModelSetting()
+    @State private var showError = false
+    
     var body: some View {
         VStack {
+            Section {
+                Stepper(value: $trainModeSetting.maxValue, in: 5...100, step: 5) {
+                    Text("Max: \(trainModeSetting.maxValue)")
+                }
+                Divider()
+            }
             Spacer()
-            Text("Setting View")
-            Spacer()
-            Divider()
+            saveBtn
         }
+        .padding(.horizontal)
+        .onAppear {
+            if let setting = TrainModelSetting.decode(data: trainModeData) {
+                trainModeSetting = setting
+            }
+        }
+        .alert("Error", isPresented: $showError, actions: {}) {
+            Text("Failed to save the settings, please try again")
+        }
+
+    }
+    
+    private var saveBtn: some View {
+        Button {
+            if let settingData = trainModeSetting.encode() {
+                trainModeData = settingData
+            } else {
+                showError = true
+            }
+        } label: {
+            Text("Save")
+                .padding(.vertical, 10)
+                .frame(maxWidth: .infinity)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(CustomColor.tintColor, lineWidth: 2)
+                )
+        }
+        .padding(.bottom)
     }
 }
 
