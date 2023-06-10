5822107c88dabe2aea57c1c480016427b2c514cc
Gray Zhen <grayzprogrammer@gmail.com>
2023-06-10 18:54:17 -0500

    BM-0004 | use @appStorage to store user settings (#4)

--- BrainMath/Views/TrainView.swift
+++ BrainMath/Views/TrainView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct TrainView: View {
+    @AppStorage("trainModeSetting") var trainModeData = TrainModelSetting().encode()!
+    @State private var trainModeSetting = TrainModelSetting()
+    
     var body: some View {
         VStack {
             Spacer()
             Text("Train View")
+            Text("\(trainModeSetting.maxValue)")
             Spacer()
             Divider()
+        }
+        .onAppear {
+            if let setting = TrainModelSetting.decode(data: trainModeData) {
+                trainModeSetting = setting
+            }
         }
     }
 }
