efe418de69ee50ecb631c78af9e8103f4ff6eb65
Gray <grayzprogrammer@gmail.com>
2023-06-19 19:27:03 -0500

    refactor the train view

---
+++ BrainMath/Helpers/TrainViewHelper.swift
@@ -0,0 +1,130 @@
+//
+//  TrainViewHelper.swift
+//  BrainMath
+//
+//  Created by G Zhen on 6/19/23.
+//
+
+import SwiftUI
+
+func displayQuestion(x: String, y: String, answer: String, type: String) -> some View {
+    return HStack(alignment: .center) {
+        ZStack {
+            RoundedRectangle(cornerRadius: 6)
+                .stroke(CustomColor.tintColor, lineWidth: 2)
+                .frame(width: 60, height: 60)
+            Text("\(x)")
+        }
+        Text(" \(type) ")
+        ZStack {
+            RoundedRectangle(cornerRadius: 6)
+                .stroke(CustomColor.tintColor, lineWidth: 2)
+                .frame(width: 60, height: 60)
+            Text("\(y)")
+        }
+        Text(" = ")
+        ZStack {
+            RoundedRectangle(cornerRadius: 6)
+                .stroke(CustomColor.tintColor, lineWidth: 2)
+                .frame(width: 60, height: 60)
+            Text("\(answer)")
+        }
+    }
+}
+
+func displayUserInput(_ input: String) -> some View {
+    return HStack {
+        Image(systemName: "pencil.line")
+            .foregroundColor(.gray)
+            .font(.headline)
+        Text("\(input.isEmpty ? "answer" : input)")
+            .opacity(input.isEmpty ? 0.4 : 1)
+        Spacer()
+    }
+    .frame(width: 280)
+    .padding()
+    .overlay(
+        RoundedRectangle(cornerRadius: 4)
+            .stroke(Color.gray, lineWidth: 1)
+            .frame(height: 50)
+    )
+}
+
+func createKeyboard(height: CGFloat, result: Binding<String>) -> some View {
+    return Grid(horizontalSpacing: 16, verticalSpacing: 16) {
+        createGridRow(1, 3, length: height/4, result: result)
+        createGridRow(4, 6, length: height/4, result: result)
+        createGridRow(7, 9, length: height/4, result: result)
+        createGridRowVariant(10, 12, length: height/4, result: result)
+    }
+}
+
+func createGridRow(_ from: Int, _ to: Int, length: CGFloat, result: Binding<String>) -> some View {
+    return GridRow {
+        ForEach(from..<to+1, id: \.self) { n in
+            Button {
+                if result.wrappedValue == "0" {
+                    result.wrappedValue = ""
+                }
+                result.wrappedValue += "\(n)"
+            } label: {
+                Text("\(n)")
+                    .frame(width: length, height: length)
+                    .border(CustomColor.tintColor)
+            }
+        }
+    }
+}
+
+func createGridRowVariant(_ from: Int, _ to: Int, length: CGFloat, result: Binding<String>) -> some View {
+    return GridRow {
+        ForEach(10..<13) { n in
+            Button {
+                switch n  {
+                case 10:
+                    result.wrappedValue = ""
+                case 11:
+                    if (result.wrappedValue != "0") {
+                        result.wrappedValue += "0"
+                    }
+                case 12:
+                    if (!result.wrappedValue.isEmpty) {
+                        result.wrappedValue.removeLast()
+                    }
+                default:
+                    break
+                }
+            } label: {
+                switch n  {
+                case 10:
+                    Image(systemName: "eraser.line.dashed")
+                        .frame(width: length, height: length)
+                        .border(CustomColor.tintColor)
+                case 11:
+                    Text("0")
+                        .frame(width: length, height: length)
+                        .border(CustomColor.tintColor)
+                case 12:
+                    Image(systemName: "arrow.backward")
+                        .frame(width: length, height: length)
+                        .border(CustomColor.tintColor)
+                default:
+                    Text("Error")
+                }
+            }
+        }
+    }
+}
+
+func displaySkipBtn() -> some View {
+    return Button {
+        
+    } label: {
+        Text("Skip")
+            .frame(width: 320, height: 50)
+            .background(
+                RoundedRectangle(cornerRadius: 0)
+                    .stroke(CustomColor.tintColor, lineWidth: 1)
+            )
+    }
+}
--- BrainMath/Views/TrainView.swift
+++ BrainMath/Views/TrainView.swift
@@ -13,141 +13,30 @@ struct TrainView: View {
     @State private var answer = ""
     
     var body: some View {
-        VStack {
-            HStack(alignment: .center) {
+        GeometryReader { geo in
+            VStack {
+                displayQuestion(x: "5", y: "?", answer: "9999", type: "+")
+                    .padding(.vertical)
+                displayUserInput(answer)
+                Spacer()
+                createKeyboard(height: geo.size.height*0.4, result: $answer)
+                Spacer()
+                displaySkipBtn()
                 ZStack {
-                    RoundedRectangle(cornerRadius: 6)
-                        .stroke(CustomColor.tintColor, lineWidth: 2)
-                        .frame(width: 60, height: 60)
-                    Text("5")
+                    Rectangle()
+                        .stroke(CustomColor.tintColor, lineWidth: 1)
+                        .frame(width: 320, height: 50)
+                    Text("Ad placement")
                 }
-                Text(" X ")
-                ZStack {
-                    RoundedRectangle(cornerRadius: 6)
-                        .stroke(CustomColor.tintColor, lineWidth: 2)
-                        .frame(width: 60, height: 60)
-                    Text("?")
-                }
-                Text(" = ")
-                ZStack {
-                    RoundedRectangle(cornerRadius: 6)
-                        .stroke(CustomColor.tintColor, lineWidth: 2)
-                        .frame(width: 60, height: 60)
-                    Text("9999")
-                }
+                Divider()
             }
-            .font(.system(size: 21))
-            .padding()
-            HStack {
-                Image(systemName: "pencil.line")
-                    .foregroundColor(.gray)
-                    .font(.headline)
-                TextField("answer", text: $answer)
-                    .disabled(true)
-            }
-            .frame(width: 280)
-            .padding()
-            .overlay(RoundedRectangle(cornerRadius: 8).stroke(.gray, lineWidth: 1))
-            .padding()
-            Grid(horizontalSpacing: 16, verticalSpacing: 16) {
-                GridRow {
-                    ForEach(1..<4) { n in
-                        Button {
-                            answer += "\(n)"
-                        } label: {
-                            Text("\(n)")
-                                .frame(width: 80,height: 80)
-                                .border(CustomColor.tintColor)
-                                .cornerRadius(8)
-                        }
-                    }
-                }
-                GridRow {
-                    ForEach(4..<7) { n in
-                        Button {
-                            answer += "\(n)"
-                        } label: {
-                            Text("\(n)")
-                                .frame(width: 80,height: 80)
-                                .border(CustomColor.tintColor)
-                                .cornerRadius(8)
-                        }
-                    }
-                }
-                GridRow {
-                    ForEach(7..<10) { n in
-                        Button {
-                            answer += "\(n)"
-                        } label: {
-                            Text("\(n)")
-                                .frame(width: 80,height: 80)
-                                .border(CustomColor.tintColor)
-                                .cornerRadius(8)
-                        }
-                    }
-                }
-                GridRow {
-                    ForEach(10..<13) { n in
-                        Button {
-                            switch n  {
-                            case 10:
-                                answer = ""
-                            case 11:
-                                if (!answer.isEmpty) {
-                                    answer += "0"
-                                }
-                            case 12:
-                                if (!answer.isEmpty) {
-                                    answer.removeLast()
-                                }
-                            default:
-                                break
-                            }
-                        } label: {
-                            switch n  {
-                            case 10:
-                                Text("Clear")
-                                    .frame(width: 80,height: 80)
-                                    .border(CustomColor.tintColor)
-                                    .cornerRadius(8)
-                            case 11:
-                                Text("0")
-                                    .frame(width: 80,height: 80)
-                                    .border(CustomColor.tintColor)
-                                    .cornerRadius(8)
-                            case 12:
-                                Text("Back")
-                                    .frame(width: 80,height: 80)
-                                    .border(CustomColor.tintColor)
-                                    .cornerRadius(8)
-                            default:
-                                Text("Error")
-                            }
-                        }
-                    }
-                }
-            }
-            .padding()
-            Spacer()
-            Button {
-                
-            } label: {
-                Text("Check")
-                    .frame(width: 320, height: 60)
-                    .background(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(CustomColor.tintColor, lineWidth: 2)
-                    )
-            }
-            Spacer()
-            Divider()
         }
+        .padding(.horizontal)
         .onAppear {
             if let setting = TrainModelSetting.decode(data: trainModeData) {
                 trainModeSetting = setting
             }
         }
-        .padding(.horizontal)
     }
 }
 
